2a9a638e0545713cb5ad01deb9a55cbb2b2b7221
Simon Mullen <joosypigeon@googlemail.com>
2023-07-31 21:16:22 +0100

    Removed unused function

--- bitboard.py
+++ bitboard.py
@@ -4,45 +4,6 @@ import chess
 import chess.pgn
 import numpy as np
 
-def extract_bitboards_and_scores(pgn_text):
-    dataset = []
-    to_play_next = chess.BLACK
-    # Create a StringIO object from the PGN text
-    pgn = io.StringIO(pgn_text)
-
-    # Iterate over all games in the PGN file
-    while True:
-        game = chess.pgn.read_game(pgn)
-        if game is None:
-            break
-
-        # Check if the game has embedded Stockfish evaluations
-        has_evaluations = any('[%eval ' in node.comment for node in game.mainline())
-        if not has_evaluations:
-            continue
-
-        # Setup initial empty chess board
-        board = chess.Board()
-
-        # Iterate through all moves of the game
-        for node in game.mainline():
-            move = node.move
-            board.push(move)
-            # Get the comment (which contains the evaluation) for this move
-            comment = node.comment
-            match = re.search(r"\[%eval (.*?)\]", comment)
-            if match is None:
-                continue
-            
-            score = float(match.group(1))
-
-            # Convert the board position to bitboards and add to list
-            bitboards = board_to_bitboards(board)
-
-            dataset.append([bitboards, to_play_next, score])
-            to_play_next = not to_play_next
-    return dataset
-
 def board_to_bitboards(board):
     bitboards = []
     for color in chess.COLORS:
